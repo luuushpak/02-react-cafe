caf05d675c75f39a2835d7fda8bea9b7105d8675
luuushpak <lushpak0111@gmail.com>
2026-03-07 01:21:36 +0200

    create vidget

---
+++ src/components/VoteStatus/VoteStatus.tsx
@@ -0,0 +1,33 @@
+import styles from "./VoteStatus.module.css";
+
+interface VoteStatsProps {
+  votes: { good: number; neutral: number; bad: number };
+  totalVotes: number;
+  positiveRate: number;
+}
+
+export default function VoteStats({
+  votes: { good, neutral, bad },
+  totalVotes,
+  positiveRate,
+}: VoteStatsProps) {
+  return (
+    <div className={styles.container}>
+      <p className={styles.stat}>
+        Good: <strong>{good}</strong>
+      </p>
+      <p className={styles.stat}>
+        Neutral: <strong>{neutral}</strong>
+      </p>
+      <p className={styles.stat}>
+        Bad: <strong>{bad}</strong>
+      </p>
+      <p className={styles.stat}>
+        Total: <strong>{totalVotes}</strong>
+      </p>
+      <p className={styles.stat}>
+        Positive: <strong>{positiveRate + "%"}</strong>
+      </p>
+    </div>
+  );
+}
